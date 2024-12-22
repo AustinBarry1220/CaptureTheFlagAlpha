4d6cdf4abf5a88fccb215dba0e96cebd6f2088db
Austin Barry <Austin.J.Barry@outlook.com>
2024-12-22 17:12:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CaptureTheFlagAlpha
+
+Developed with Unreal Engine 5
---
+++ Source/CaptureTheFlagAlpha/CaptureTheFlagAlpha.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CaptureTheFlagAlpha.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CaptureTheFlagAlpha, "CaptureTheFlagAlpha" );
